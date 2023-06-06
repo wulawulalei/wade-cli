0acd7a1b8366e6cbc1e534a46f76fd0d662a0b8e
wulawulalei <806913282@qq.com>
2023-06-06 23:43:40 +0800

    feat:添加删除文件夹函数以及create时选择是否merge

--- command/create.js
+++ command/create.js
@@ -1,31 +1,43 @@
 const inquirer = require('inquirer')
 
-const frame = [{
-        name: 'vue2'
-    },
-    { name: 'vue3' },
-    {
-        name: ' react'
-    }
-]
+const fs = require('fs')
 
-const questions = [{
-    type: 'list',
-    name: 'frame',
-    message: 'choice project type',
-    choices: frame
-}]
+const path = require('path')
 
-const { log, error } = require('../lib/utils/log')
+const remove = require('../lib/remove')
+
+const clearConsole = require('../lib/utils/clear-console')
+
+const { log } = require('../lib/utils/log')
 
 async function create(name) {
-    await inquirer.prompt(questions, {})
-        .then(answers => {
-            log(answers);
-        })
-        .catch(e => {
-            error(e)
-        })
+    const cwd = path.join(process.cwd(), name)
+
+    if (fs.existsSync(cwd)) {
+        clearConsole()
+        const { choice } = await inquirer.prompt([{
+            type: 'list',
+            name: 'choice',
+            message: 'Do you want to merge the local project with the same name',
+            choices: [{
+                name: 'YES',
+                value: 'merge'
+            }, {
+                name: 'NO',
+                value: 'overWrite'
+            }]
+        }])
+
+        // 选择了覆盖则先删除掉
+        if (choice === 'overWrite') {
+            log(`removing file from ${cwd}`);
+            remove(cwd)
+        }
+
+    }
+
+    // 下载文件流程（选择npm还是yarn，是否需要eslint，balel等）
+
 }
 
 module.exports = create
